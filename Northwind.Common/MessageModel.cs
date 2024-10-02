6ca5bfdd7bf8dd0a1e54150a7c96e3d498d38522
a.mirzaei <ashkanmv@gmail.com>
2024-10-02 19:02:36 +0330

    Add project files.

---
+++ Northwind.Common/MessageModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Northwind.Common
+{
+    public class MessageModel
+    {
+        public string From { get; set; } = null!;
+        public string To { get; set; } = null!;
+        public string? Body { get; set; }
+    }
+}
---
+++ Northwind.Common/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Northwind.Common
+{
+    public class UserModel
+    {
+        public string Name { get; set; } = null!;
+        public string ConnectionId { get; set; } = null!;
+        public string? Groups { get; set; } // comma-separated list
+    }
+}
